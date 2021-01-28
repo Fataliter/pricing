4c2db608b376986e01fcd69039d4a3800a60864e
fatal <fatal@heh>
2021-01-28 21:56:47 +0100

    almost working - nopr

--- finder/platforms/steam.py
+++ finder/platforms/steam.py
@@ -4,7 +4,8 @@ from bs4 import BeautifulSoup as bs
 GAMES = {
     'Trine 3': ['319910/Trine_3_The_Artifacts_of_Power/'],
     'Trine 4': ['690640/Trine_4_The_Nightmare_Prince/', '1444560/Trine_4_Melody_of_Mystery/'],
-    'Portal 2': ['620/Portal_2/']
+    'Portal 2': ['620/Portal_2/'],
+    'Grim Dawn': ['219990/Grim_Dawn/']
 }
 
 
@@ -24,7 +25,15 @@ class Steam:
                 name_from_site = content.find('div', { 'class': 'apphub_AppName' }).getText().strip()
                 prices[game][name_from_site] = {}
 
-                purchase = content.find('div', { 'class': 'game_area_purchase_game_wrapper' })
+                purchases = content.find_all('div', { 'class': 'game_area_purchase_game_wrapper' })
+
+                purchase = purchases[0]
+                for p in purchases:
+                    name = p.find('h1').getText().strip()
+                    if len(name.replace('Buy ', '')) <= len(name_from_site):
+                        purchase = p
+                        break
+
                 discount_block = purchase.find('div', { 'class': 'game_purchase_discount' })
 
                 if discount_block is not None:
